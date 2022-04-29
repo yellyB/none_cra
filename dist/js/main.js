20aa37638b36942ecdb333afb15ef1e345f0abde
bom\bomi <kk3p1q1@gmail.com>
2022-04-29 14:20:22 +0900

    polyfill 테스트용 코드 추가

--- dist/js/main.js
+++ dist/js/main.js
@@ -7,3 +7,12 @@ console.log((0, _lib.power)(_lib.pi, _lib.pi));
 var f = new _lib.Foo();
 console.log(_lib.pi);
 console.log(_lib.pi);
+
+// polyfill이 필요한 코드
+console.log(
+  new Promise((resolve, reject) => {
+    setTimeout(() => resolve(1), 100);
+  })
+);
+console.log(Object.assign({}, { x: 1 }, { y: 2 }));
+console.log(Array.from([1, 2, 3], (v) => v + v));
