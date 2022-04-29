3922813d534520b4c5ffdbb365f759cc71ef23ec
bom\bomi <kk3p1q1@gmail.com>
2022-04-29 13:25:16 +0900

    ES6이상 문법인것 주석 추가

--- src/js/lib.js
+++ src/js/lib.js
@@ -1,13 +1,14 @@
-export const pi = Math.PI;
+export const pi = Math.PI; // ES6모듈
 
 export function power(x, y) {
-  return x ** y;
+  return x ** y; // ES7 지수연산자
 }
 
 export class Foo {
-  #private = 10;
+  #private = 10; // 클래스 필드 정의 제안
 
   foo() {
+    // 객체 Rest/Spead 프로퍼티 제안
     const { a, b, ...x } = { ...{ a: 1, b: 2 }, c: 3, d: 4 };
     return { a, b, x };
   }
